888201848c093dc382f3d4cb0b35d25fc117551c
CodeMind <miguelsaffier@gmail.com>
2023-03-16 06:41:34 -0300

    Beroepsproduct

--- src/main/webapp/js/product.js
+++ src/main/webapp/js/product.js
@@ -1,7 +1,9 @@
-
+const img1 = document.getElementById("product1");
+img1.src = "http://localhost:8080/logistiekWebApp_war_exploded/api/product/get_product_image/102";
+img1.alt = "double washing sand";
 
 function displayImageFromAPI(id) {
-    fetch('http://localhost:8080/logistiekWebApp_war_exploded/api/product/get_product_image/'+id)
-
-    // ik moet dit nog afronden, maar api werkt
+   return  fetch('http://localhost:8080/logistiekWebApp_war_exploded/api/product/get_product_image/'+id);
 }
+
+
